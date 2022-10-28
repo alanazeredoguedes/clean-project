958f220da1b6b62462fb6722f9d4b6be1da9294a
Alan Guedes <alanguedes00@live.com>
2022-10-27 23:59:15 -0300

    updast

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'home_')]
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'init')]
+    public function redirectAdmin(): Response
+    {
+        return $this->json(['status' => true]);
+    }
+
+}
