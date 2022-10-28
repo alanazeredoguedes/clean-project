4260d1dad294536ca37abbd082645e5a4bbb02ac
Alan Guedes <alanguedes00@live.com>
2022-10-28 08:38:23 -0300

    update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/', name: 'home_')]
+#[Route('/app', name: 'home_')]
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'init')]
